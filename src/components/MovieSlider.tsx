47c58545c6894e10543dfdb2217faaf0e3a2d407
DESKTOP-9VU9I7S\Rai <ajipunk008@gmail.com>
2023-08-15 09:56:26 +0700

    feat axios

---
+++ src/components/MovieSlider.tsx
@@ -0,0 +1,5 @@
+export default function MovieSlider() {
+    return (
+        <div>Movie Slider</div>
+    )
+}
---
+++ src/utils/api.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const baseUrl = "https://api.themoviedb.org";
+const token =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZWMwZDM4NjYwNTM2OTQxMjZlMDU0Y2I4MTE2ODlhMSIsInN1YiI6IjYwMzRiNDUxYTE0ZTEwMDA0MGNmNzMzOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.A4cEGxl1Xc0KWe4Ejh228eA_KCseyLQVucqMXnEzD4o";
+
+const api = axios.create({
+  baseURL: baseUrl,
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
+});
+
+// handle axios error
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+export default api;
